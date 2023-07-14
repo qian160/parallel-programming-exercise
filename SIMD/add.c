65555fcf21ccbea16ac36ee6e6b90a30f48e679f
nakashima <1601315694@qq.com>
2023-07-15 03:56:37 +0800

    update

--- SIMD/add.c
+++ SIMD/add.c
@@ -1,14 +1,12 @@
 // lscpu | grep avx
 // gcc -mavx2
-// data must be aligned, or these intrincs may cause a seg fault
 #include<immintrin.h>
 #include<stdio.h>
 #include<assert.h>
+#include<stdalign.h>
 
 void asm_test(){
-        double a[4] __attribute__((aligned(32)))={1,2,3,4} ,
-                b[4] __attribute__((aligned(32))) ={5,6,7,8},
-                c[4] __attribute__((aligned(32))) ={0} ;
+        alignas(32) double a[4] = {1,2,3,4}, b[4] = {5,6,7,8}, c[4] = {0} ;
                 __asm__ volatile(
                         "vmovapd (%1),%%ymm0\n"
                         "vmovapd (%2),%%ymm1\n"
@@ -34,7 +32,8 @@ int array_sum_int_avx512(int *array, int n){
 	return _mm512_reduce_add_epi32(vec);	// more convenient, but only supported by avx512, not avx2
 }
 */
-int array_sum_int_avx2(int *array, int n){
+// aligned tends to be faster
+int array_sum_int_avx2_aligned(int *array, int n){
 	assert((n & 0b111) == 0);
 	int sum = 0;
 	__m256i vec = _mm256_load_si256((__m256i*)array);
@@ -50,11 +49,29 @@ int array_sum_int_avx2(int *array, int n){
 	return sum;
 }
 
+int array_sum_int_avx2_unaligned(int *array, int n){
+	assert((n & 0b111) == 0);
+	int sum = 0;
+	__m256i vec = _mm256_loadu_si256((__m256i*)array);
+	for(int i = 8; i < n; i += 8)
+	{
+		__m256i data = _mm256_loadu_si256((__m256i*)(array + i));
+		vec = _mm256_add_epi32(vec, data);
+	}
+	int sum_array[8];
+	_mm256_storeu_si256((__m256i*)sum_array, vec);
+	for(int i = 0; i < 8; i++)
+		sum += sum_array[i];
+	return sum;
+}
 int main()
 {
-	int a[1024] __attribute__((aligned(32)));
+	//int a[1024] __attribute__((aligned(32)));
+	alignas(32) int a[1024];
+	int b[1024];
 	for(int i = 0; i < 1024; i++)
-		a[i] = 1;
+		a[i] = b[i] = 1;
 	asm_test();
-	printf("sum of a = %d\n", array_sum_int_avx2(a, 1024));
+	printf("sum of a = %d\n", array_sum_int_avx2_aligned(a, 1024));
+	printf("sum of a = %d\n", array_sum_int_avx2_unaligned(a, 1024));
 }
